566c90923a738f2a2f502cd06ec11dede2b6ecec
Gene Torres <einobaka@gmail.com>
2020-04-18 23:07:56 -0700

    added comments in script.js explaining the order

--- Assets/script.js
+++ Assets/script.js
@@ -9,16 +9,17 @@ function writePassword() {
 
 }
 
-// First method to confirm if the characters are set between 8 and 128
 function generatePassword() {
+  // First prompt for character length between 8 and 128.
   var askLength = prompt('How long would you like your password? Pick between 8-128 characters.');
-
+  // Second set of confirms for character selection.
   if (askLength >= 8 && askLength <= 128) {
     var askLowercase = confirm('Would you like lowercase letters?');
     var askUppercase = confirm('Would you like uppercase letters?');
     var askNumeric = confirm('Would you like numeric characters?');
     var askSpecial = confirm('Would you like special characters?');
   }
+  // Reject right away if user does not meet the length.
   else {
     alert('Password does not meet parameters; please choose the length between 8 & 128.')
   }
@@ -47,6 +48,7 @@ function generatePassword() {
     var special = '';
   }
   if (askLowercase === true || askUppercase === true || askNumeric === true || askSpecial === true) {
+    // Alert the confirms from previous selection.
     var promptResult = [
       "These are your following parameters: ",
       "\n\n Desired password length: " + askLength,
@@ -56,17 +58,18 @@ function generatePassword() {
       "\n You picked special characters: " + askSpecial
     ];
     alert(promptResult);
-
+    // Randomize and generate the password via for()loop
     var characters = lowercase + uppercase + numeric + special;
     var randomize = [];
     for (var i = 0; i < askLength; i++) {
       randomize += characters[Math.floor(Math.random() * characters.length)];
     };
-
+    // Return the final result to generatePassword();
     return randomize;
   }
+  // If user doesn't select enough characters, return of reject will happen instead.
   else {
-    alert('Password does not meet parameters; please choose at least one character type.')
+    return alert('Password does not meet parameters; please choose at least one character type.')
   }
 
 };
